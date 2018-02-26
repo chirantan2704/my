f6e0a21f7f9942f7f68f7b3b03ffcf8a7db9e84d
pathachi <chirantan.pathak@gmail.com>
2018-02-26 16:54:53 +0530

    subscription bean

---
+++ src/main/java/com/mench/bean/SubscriptionBundle.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mench.bean;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author pathachi
+ */
+public class SubscriptionBundle {
+    
+   int subscriptionId;			
+   int bundleId;			
+   double cost;			
+   boolean isActive;			
+   LocalDateTime addedDate;			
+   LocalDateTime deactivatedDate;			
+   int productId;			
+   int units;			
+   double totalCost;
+
+    public int getSubscriptionId() {
+        return subscriptionId;
+    }
+
+    public void setSubscriptionId(int subscriptionId) {
+        this.subscriptionId = subscriptionId;
+    }
+
+    public int getBundleId() {
+        return bundleId;
+    }
+
+    public void setBundleId(int bundleId) {
+        this.bundleId = bundleId;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public boolean isIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    public LocalDateTime getAddedDate() {
+        return addedDate;
+    }
+
+    public void setAddedDate(LocalDateTime addedDate) {
+        this.addedDate = addedDate;
+    }
+
+    public LocalDateTime getDeactivatedDate() {
+        return deactivatedDate;
+    }
+
+    public void setDeactivatedDate(LocalDateTime deactivatedDate) {
+        this.deactivatedDate = deactivatedDate;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getUnits() {
+        return units;
+    }
+
+    public void setUnits(int units) {
+        this.units = units;
+    }
+
+    public double gettotalCost() {
+        return totalCost;
+    }
+
+    public void setTotal_cost(double totalCost) {
+        this.totalCost = totalCost;
+    }
+}
