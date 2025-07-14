46f199a981d7b674edc05960f51f5e45f20e0eac
chirantan2704 <36704273+chirantan2704@users.noreply.github.com>
2025-07-14 12:17:20 +0200

    Update BundleServiceImpl.java

--- src/main/java/com/mench/service/BundleServiceImpl.java
+++ src/main/java/com/mench/service/BundleServiceImpl.java
@@ -47,7 +47,7 @@ public class BundleServiceImpl implements BundleService {
 
     @Override
     public List<Bundle> findAll() {
-           List<Bundle> bundle= bundleDao.findAll();
+    //       List<Bundle> bundle= bundleDao.findAll();
     //    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
       return bundle;
     }
